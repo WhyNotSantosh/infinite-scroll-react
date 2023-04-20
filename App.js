0b95e3440600c8eff133a15936bd1a2d7df75325
WhyNotSantosh <san_sriramadasu@yahoo.com>
2023-04-20 11:14:26 +0530

    updated code for edge cases

--- App.js
+++ App.js
@@ -5,14 +5,17 @@ import InfiniteScroll from "./src/component/InfiniteScroll";
 const App = () => {
     const [apiQuery, setApiQuery] = useState('');
     const [data, setData] = useState([]);
+    const [noData, setNoData] = useState(false);
     const handleSearchInput = (e) => {
-        setApiQuery(e.target.value)
+        setData([]);
+        setNoData(false);
+        setApiQuery(e.target.value);        
         if (e.target.value.length === 0) {
             setData([]);
         }
     }
     const renderItem = ({ title, key }, ref) => {
-        return <div key={key + "_" + title} ref={ref} className="text-lg text-cyan-800 box-border flex-wrap w-54 p-4 border-4 rounded-md border-orange-300 overflow-x-auto">{title}</div>
+        return <div key={key + "_" + title} ref={ref} className="text-sm text-cyan-800 box-border flex-wrap p-4 border-4 rounded-md border-orange-300 break-words">{title}</div>        
     }
 
     // Function to fetch the data from api
@@ -25,8 +28,11 @@ const App = () => {
                         page: pageNumber
                     }));
                     const data = await promise.json();
-                    resolve();
-                    setData((prevData) => [...prevData, ...data.docs]);
+                    resolve();          
+                    if(data.docs.length === 0){
+                        setNoData(true);
+                    }
+                    setData((prevData) => [...prevData, ...data.docs]);                                                        
                 }
                 catch (err) {
                     console.log('Error fetching data', err);
@@ -45,6 +51,7 @@ const App = () => {
                 getData={getData}
                 listData={data}
                 apiQuery={apiQuery}
+                noData={noData}
             />}
         </>
     )
--- src/component/InfiniteScroll.js
+++ src/component/InfiniteScroll.js
@@ -1,61 +1,60 @@
 import { useEffect, useRef, useState } from "react";
-import logo from "../../assets/loading-loader.gif"
+import logo from "../../assets/loading-loader.gif";
 const InfiniteScroll = (props) => {
-    const { renderListItem, getData, listData, apiQuery } = props;
-    const pageNumber = useRef(1);
-    const [loading, setLoading] = useState(false);
-    const observer = useRef(null);
-    const lastElementOberver = (node) => {
-        if (loading) {
-            return
-        }
-        if (observer.current) {
-            observer.current.disconnect();
-        }
-        observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting) {
-                pageNumber.current += 1;
-                fetchData();
-            }
-        })
-        if (node) {
-            observer.current.observe(node);
-        }
+  const { renderListItem, getData, listData, apiQuery, noData } = props;
+  const pageNumber = useRef(1);
+  const [loading, setLoading] = useState(false);
+  const observer = useRef(null);
+  const lastElementOberver = (node) => {
+    if (loading) {
+      return;
     }
-
-    const renderList = () => {
-        return listData.map((item, index) => {
-            if (index === listData.length - 1) {
-                return renderListItem(item, lastElementOberver)
-            }
-            return renderListItem(item, null)
-        });
+    if (observer.current) {
+      observer.current.disconnect();
     }
-    const fetchData = () => {
-        setLoading(true);
-        getData(apiQuery, pageNumber.current)
-            .finally(() => {
-                setLoading(false);
-            });
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        pageNumber.current += 1;
+        fetchData();
+      }
+    });
+    if (node) {
+      observer.current.observe(node);
     }
-    useEffect(() => {
-        const debounceTimer = setTimeout(() => {
-            fetchData();
-        }, 300);
+  };
 
-        return () => {
-            clearTimeout(debounceTimer);
-        }
+  const renderList = () => {
+    if (noData) {
+      return <span className="m-auto col-span-4 font-bold text-xl text-red-700">No such results found.</span>;
+    }
+    return listData.map((item, index) => {
+      if (index === listData.length - 1) {
+        return renderListItem(item, lastElementOberver);
+      }
+      return renderListItem(item, null);
+    });
+  };
+  const fetchData = () => {
+    setLoading(true);
+    getData(apiQuery, pageNumber.current).finally(() => {
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      fetchData();
+    }, 1000);
 
-    }, [apiQuery]);
-    return (
-        <>
-            <div className="grid grid-cols-4 gap-4 p-2 m-2">
-                {renderList()}
-            </div>
-            {loading && <img src={logo} className="w-52 m-auto" />}
-        </>
-    )
-}
+    return () => {
+      clearTimeout(debounceTimer);
+    };
+  }, [apiQuery]);
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-4 p-2 m-2">{renderList()}</div>
+      {loading && <img src={logo} className="w-52 m-auto" />}
+    </>
+  );
+};
 
 export default InfiniteScroll;
